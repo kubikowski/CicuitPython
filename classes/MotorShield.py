5861382f86182069fce6667913ddbce357069880
kubikowski <44616042+kubikowski@users.noreply.github.com>
2019-09-15 22:28:34 -0400

    updated steps

--- classes/MotorShield.py
+++ classes/MotorShield.py
@@ -6,8 +6,8 @@ class MotorShield(object):
         self.kit = am.MotorKit()
 
     def steps(self, steps, **kwargs):
-        direction = kwargs[direction] if 'direction' in kwargs.keys() else stepper.FORWARD
-        style = kwargs[style] if 'style' in kwargs.keys() else style = stepper.SINGLE
+        direction = kwargs['direction'] if 'direction' in kwargs.keys() else stepper.FORWARD
+        style = kwargs['style'] if 'style' in kwargs.keys() else stepper.SINGLE
 
         for i in range(steps):
             self.kit.stepper1.onestep(direction=direction, style=style)
