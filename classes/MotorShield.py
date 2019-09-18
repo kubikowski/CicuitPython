b2d02b47e127c740191eebe267d0c3c120627fe6
kubikowski <44616042+kubikowski@users.noreply.github.com>
2019-09-18 00:34:31 -0400

    simplified motorShield

--- classes/MotorShield.py
+++ classes/MotorShield.py
@@ -5,12 +5,16 @@ class MotorShield(object):
     def __init__(self):
         self.kit = am.MotorKit()
 
-    def steps(self, steps, **kwargs):
-        direction = kwargs['direction'] if 'direction' in kwargs.keys() else stepper.FORWARD
-        style = kwargs['style'] if 'style' in kwargs.keys() else stepper.SINGLE
-
+    def step1(self, steps, **kwargs):
         for i in range(steps):
-            self.kit.stepper1.onestep(direction=direction, style=style)
+            self.kit.stepper1.onestep(**kwargs)
+
+    def step2(self, steps, **kwargs):
+        for i in range(steps):
+            self.kit.stepper2.onestep(**kwargs)
+
+    def steps(self, steps, *args, **kwargs):
+        self.step2(steps, **kwargs) if len(args) == 1 and args[0] == 2 else self.step1(steps, **kwargs)
 
     def release(self):
         self.kit.stepper1.release()
--- controllers/motorShieldController.py
+++ controllers/motorShieldController.py
@@ -1,10 +1,13 @@
 from MotorShield import MotorShield
 from adafruit_motor import stepper
+import time
 
 # Setup
 shield = MotorShield()
 
 # Methods
 def strikeGong():
-    shield.steps(300, style=stepper.MICROSTEP)
+    shield.steps(200, style=stepper.MICROSTEP)
+    time.sleep(0.1)
+    shield.steps(75, style=stepper.DOUBLE, direction=stepper.BACKWARD)
     shield.release()
